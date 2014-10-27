0d9bc845c9ce607388f8ebc9cadf1226690b2a4b
Jeremy Chambers <jeremy@thehipbot.com>
2014-10-27 00:26:39 -0500

    Changed video to view. Added code to retrieve course section for index and video for view

--- controllers/recordings.py
+++ controllers/recordings.py
@@ -2,12 +2,17 @@
 #This is the controller for the class recordings module of LMS299
 
 def index():
+    section_id = request.args(0,cast=int)
+    videos = db(db.recording.course_id==section_id).select()
     youtube_id='dQw4w9WgXcQ'
-    return locals()
+    return dict(youtube_id=youtube_id, videos=videos)
 
-def video():
+def view():
+    video_id = request.args(0,cast=int)
+    video = db(db.recording.id==video_id).select().first()
+    return dict(video=video)
 
-    return dict()
-
+@auth.requires_login()
 def create():
+
     return  dict()
