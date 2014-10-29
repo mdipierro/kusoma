ea26b2032bbb0cebf8fd9d917c6acb958ea682c6
John Lofgren <john.lofgren@gmail.com>
2014-10-29 02:37:14 +0000

    Made teacher/student check more thorough in index. Fixed some redirect issues.

--- controllers/recordings.py
+++ controllers/recordings.py
@@ -6,18 +6,22 @@ def index():
     section_id = request.args(0,cast=int)
     section=db(db.course_section.id == section_id).select().first()
     if not section: redirect(URL('default','index'))
-    if not is_user_student(section_id) or not is_user_student(section_id):
-        redirect(URL('section', args=section_id))
     videos = db(db.recording.course_id==section_id).select()
-    return dict(section=section, videos=videos)
+    if is_user_student(section_id):
+        is_teacher=False
+    elif is_user_teacher(section_id):
+        is_teacher=True
+    else:
+        redirect(URL('default','section', args=section_id))
+    return dict(section=section, videos=videos, is_teacher=is_teacher)
 
 @auth.requires_login()
 def view():
     video_id = request.args(0,cast=int)
     video = db(db.recording.id==video_id).select().first()
     section_id = video.course_id
-    if not is_user_student(section_id) or not is_user_student(section_id):
-        redirect(URL('section', args=section_id))
+    if not is_user_student(section_id) or not is_user_teacher(section_id):
+        redirect(URL('default','section', args=section_id))
     return dict(video=video)
 
 @auth.requires_login()
