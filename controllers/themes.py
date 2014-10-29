600ac05a99c2e0af5ec9c1eb521709212d6834c5
groos <ntgroos@gmail.com>
2014-10-29 09:49:27 -0500

    Added controller to set current session theme

--- controllers/themes.py
+++ controllers/themes.py
@@ -2,10 +2,26 @@
 
 def index():
 
-    session.flash = "Select a theme for your LMS"
+    #session.flash = "Select a theme for your LMS"
     
-    themes = SQLFORM.grid(db.theme.name))
-    themes.selectable = True
+    rows = db(db.theme.name).select()
 
-    return dict(themes = themes)
+    return dict(rows = rows)
 
+
+def redirect():
+
+    '''
+    Build the url to the css style page from arg, 
+    and assign it to session.current_theme
+    '''
+    static_subfolder = request.args(0)
+    css_filename = request.args(1)
+    
+    css_pathname = static_subfolder + '/'  + css_filename
+    session.current_theme = URL('static', css_pathname)
+
+    ### redirect in 10 seconds, or give option to go back and choose different theme
+    #redirect('http://www.google.com')
+
+    return dict()
