47ef340e6b2bbe8e471bf3791cd82aabba6d15ff
ArthurWang9 <wangqk86@gmail.com>
2014-11-10 14:31:20 -0600

    Update notes.py

--- controllers/notes.py
+++ controllers/notes.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 # try something like
+import time
+
 def index():
     return dict(message="hello just testing!!")
+
+def get_all_notes():
+    return db().select(db.note_main.All)
+
+def add_note(course_id, tag):
+    db.notes_main.insert(course_id = course_id, tag = tag, create_on = time.time())
+    db.commit()
