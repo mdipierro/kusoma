65b092c3f6eef6450f478f5248deace95c6aeeb9
John Lofgren <john.lofgren@gmail.com>
2014-11-17 02:05:26 +0000

    Removed recordings view()

--- controllers/recordings.py
+++ controllers/recordings.py
@@ -39,20 +39,6 @@ def section():
     return dict(section=section, videos=videos, is_teacher=is_teacher, start_data=start_data)
 
 @auth.requires_login()
-def view():
-    video_id = request.args(0,cast=int)
-    video = db(db.recording.id==video_id).select().first()
-    if not video: redirect(URL('default','index'))
-        
-    section_id = video.course_id
-    if not is_user_student(section_id) and not is_user_teacher(section_id):
-        redirect(URL('default','section', args=section_id))
-
-    add_section_menu(section_id)
-    
-    return dict(video=video)
-
-@auth.requires_login()
 def edit():
     """
     Edit an entry in the recording database.
