2dabd874e5339b7b3e08aac13280a33b316f54b9
ZhaoYan <Bryan@pwiemerlap.cti.depaul.edu>
2014-11-18 16:58:01 -0600

    modify the index page and do some change on the create note page

--- models/menu_note.py
+++ models/menu_note.py
@@ -1,5 +1,5 @@
 response.menu += [
-    (T('Notes'), False, URL('notes', ''), [
+    (T('Notes'), False, URL('notes', '#'), [
         (T('All Notes'), False, URL('notes', 'index'), []),
         (T('My Subscription'), False, URL('notes', 'mysubscriptions'), []),
         (T('Notifications'), False, URL('notes', 'notifications'), [])
