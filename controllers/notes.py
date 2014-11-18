fe694d4830554753e109717a49c0c56cffc4c678
ArthurWang9 <wangqk86@gmail.com>
2014-11-18 00:44:59 -0600

    Update notes.py
    fixed  .max()

--- controllers/notes.py
+++ controllers/notes.py
@@ -56,7 +56,7 @@ def get_my_note_list(user_id):
     query = (db.note_main.id == db.note_version.note_id
             )&(db.note_user_note_relation.note_id == user_id
             )&(db.note_main.id == db.note_user_note_relation.note_id
-            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(max(db.note_version.modify_on)))
+            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(db.note_version.modify_on.max()))
     rows = db(query).select(db.note_version.title, db.note_main.create_on, db.note_main.create_by, db.note_version.modify_on, db.note_version.modify_by, db.note_user_note_relation.user_id)
     return dict(rows=rows)
 
@@ -65,7 +65,7 @@ def get_note_list_beta(search_content):
     query = (db.note_main.id == db.note_version.note_id
             )&(db.note_main.id == db.note_user_note_relation.note_id
             )&(db.note_version.note_content.upper().like(search_content)
-            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(max(db.note_version.modify_on)))
+            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(db.note_version.modify_on.max()))
     rows = db(query).select(db.note_version.note_id, db.note_version.title, db.note_main.create_on, db.note_main.create_by, db.note_version.modify_on, db.note_version.modify_by, db.note_user_note_relation.user_id)
     return dict(rows=rows)
     
@@ -88,7 +88,7 @@ def get_relevant_list(note_id):
     
     query_relevant_note = (db.note_main.id == db.note_version.note_id
             )&(db.note_main.id in (db(query_note_id).select(db.note_tag.note_id))
-            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(max(db.note_version.modify_on)))
+            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(db.note_version.modify_on.max()))
             
     rows = db(query_relevant_note).select(db.note_version.title)
     return dict(rows=rows)
@@ -96,7 +96,7 @@ def get_relevant_list(note_id):
 def get_note_content(note_id):
     query = (db.note_main.id == db.note_version.note_id
             )&(db.note_main.id == note_id
-            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(max(db.note_version.modify_on)))
+            )&(db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(db.note_version.modify_on.max()))
     rows = db(query).select(db.note_version.note_content)
     return dict(rows=rows)
 
