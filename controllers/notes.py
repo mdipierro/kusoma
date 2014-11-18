6ee72be8e61da2c15f8beeadc78e6c851ec6d671
ArthurWang9 <wangqk86@gmail.com>
2014-11-17 21:39:36 -0600

    Update notes.py
    beta1.2

--- controllers/notes.py
+++ controllers/notes.py
@@ -60,6 +60,15 @@ def get_my_note_list(user_id):
     rows = db(query).select(db.note_version.title, db.note_main.creat_on, db.note_main.create_by, db.db.note_version.modify_on, db.db.note_version.modify_by, db.note_user_note_relation.user_id)
     return dict(rows=rows)
 
+def search_note_list(search_content):
+    search_content = "%" + search_content.upper().strip() + "%"
+    query = (db.note_main.id == db.note_version.note_id
+            )&(db.note_main.id == db.note_user_note_relation.note_id
+            )&(db.note_version.note_content.upper().like(search_content)
+            )&(db.db.note_version.modify_on == db(db.note_main.id == db.note_version.note_id).select(max(db.note_version.modify_on)))
+    rows = db(query).select(db.note_version.note_id, db.note_version.title, db.note_main.creat_on, db.note_main.create_by, db.note_version.modify_on, db.note_version.modify_by, db.note_user_note_relation.user_id)
+    return dict(rows=rows)
+
 def get_all_history_versions(note_id):
     query = (db.note_version.note_id == note_id)
     rows = db(query).select(db.note_version.title, db.note_version.modify_by, db.note_version.modify_on)
