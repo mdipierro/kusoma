66aa8f2f47a4e9d399219c2afcc88045e794d2b2
jjunk <junky8687@gmail.com>
2014-11-23 19:50:09 -0600

    namespacing group chat JS
    JavaScript code now have of "lms299.groupchat". jQuery's onRead() function
    is now leveraged to add the our JS to Google Hangout's onClientReady()
    callback. Before, Google's client.js library was being used.

--- static/js/group_chat.js
+++ static/js/group_chat.js
@@ -1,29 +1,37 @@
-function onClientReady() {
-  // When API is ready...                                                         
-  gapi.hangout.onApiReady.add(
-    function(eventObj) {
-      sendHangoutsUrl();
+var lms299 = {
+  groupchat: {
+    onClientReady: function() {
+      console.log("onClientReady()");
+      gapi.hangout.onApiReady.add(
+        function(eventObj) {
+        sendHangoutsUrl();
 
-      document.getElementById('showParticipants').style.visibility = 'visible';
+        document.getElementById('showParticipants').style.visibility = 'visible';
 
-      var pageUrl = window.location.search.substring(1);
-      var urlParams = pageUrl.split('&');
-      console.log(urlParams);
-      for (var i = 0; i < urlParams.length; i++) {
-        var param = urlParams[i].split('=');
-        if (param[0] == 'gd') {
-          alert (param[1]);
+        var pageUrl = window.location.search.substring(1);
+        var urlParams = pageUrl.split('&');
+        console.log(urlParams);
+        for (var i = 0; i < urlParams.length; i++) {
+          var param = urlParams[i].split('=');
+          if (param[0] == 'gd') {
+            alert (param[1]);
+          }
         }
-      }
 
-      function sendHangoutsUrl() {
-        $.ajax({
-          contentType: 'application/json',
-          type: 'POST',
-          url: "http://127.0.0.1:8000/lms299/groupchat/hagouts_url_for_session.json",
-          data: '{ "sessionId": "1", "hangoutsUrl ": "' + gapi.hangout.getHangoutUrl() + '"}',
-          dataType: 'json'
-        });
-      }
-  });
+        function sendHangoutsUrl() {
+          $.ajax({
+            contentType: 'application/json',
+            type: 'POST',
+            url: "http://127.0.0.1:8000/lms299/groupchat/hagouts_url_for_session.json",
+            data: '{ "sessionId": "1", "hangoutsUrl ": "' + gapi.hangout.getHangoutUrl() + '"}',
+            dataType: 'json'
+          });
+        }
+      });
+    }
+  }
 }
+
+$(document).ready(function () {
+  lms299.groupchat.onClientReady();
+});
