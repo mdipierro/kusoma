385cc4517a9ac1d75db99d437a51cce2be954efe
jjunk <junky8687@gmail.com>
2014-11-23 18:26:58 -0600

    sending back Hangouts URL
    When the Google Hangout app loads, the URL is now sent back to the web2py
    instance with an AJAX call.

--- static/js/group_chat.js
+++ static/js/group_chat.js
@@ -31,35 +31,38 @@ function showParticipants() {
 function onClientReady() {
   // When API is ready...                                                         
   gapi.hangout.onApiReady.add(
-      function(eventObj) {
-        document.getElementById('showParticipants')
-          .style.visibility = 'visible';
+    function(eventObj) {
+      sendHangoutsUrl();
+
+      document.getElementById('showParticipants').style.visibility = 'visible';
         
-        gapi.hangout.onParticipantsAdded.add(function(e) {
-          console.log(e);
-        });
+      gapi.hangout.onParticipantsAdded.add(function(e) {
+        console.log(e);
+      });
         
-        gapi.hangout.data.onMessageReceived.add(function(e) {
-          console.log("onMessageReceived()");
-          $('#msg').html("test: " + e);
-        });
-        
+      gapi.hangout.data.onMessageReceived.add(function(e) {
+        console.log("onMessageReceived()");
+        $('#msg').html("test: " + e);
+      });
+
+      var pageUrl = window.location.search.substring(1);
+      var urlParams = pageUrl.split('&');
+      console.log(urlParams);
+      for (var i = 0; i < urlParams.length; i++) {
+        var param = urlParams[i].split('=');
+        if (param[0] == 'gd') {
+          alert (param[1]);
+        }
+      }
+
+      function sendHangoutsUrl() {
         $.ajax({
           contentType: 'application/json',
           type: 'POST',
           url: "http://127.0.0.1:8000/lms299/groupchat/hagouts_url_for_session.json",
-          data: '{ "name": "Test", "location": "Chicago" }',
+          data: '{ "sessionId": "1", "hangoutsUrl ": "' + gapi.hangout.getHangoutUrl() + '"}',
           dataType: 'json'
-        })
-
-        var pageUrl = window.location.search.substring(1);
-        var urlParams = pageUrl.split('&');
-        console.log(urlParams);
-        for (var i = 0; i < urlParams.length; i++) {
-          var param = urlParams[i].split('=');
-          if (param[0] == 'gd') {
-            alert (param[1]);
-          }
-        }
-      });
+        });
+      }
+  });
 }
