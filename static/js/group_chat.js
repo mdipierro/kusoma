a72ae1781615f115478d2579a8eef8d2084bd06c
jjunk <junky8687@gmail.com>
2014-11-24 20:46:46 -0600

    user chat settings
    Added method to set a user's Hangouts settings such as if they want the
    mic or camera disabled.

--- static/js/group_chat.js
+++ static/js/group_chat.js
@@ -1,33 +1,37 @@
 var lms299 = {
   groupchat: {
     onClientReady: function() {
-      console.log("onClientReady()");
       gapi.hangout.onApiReady.add(
         function(eventObj) {
         sendHangoutsUrl();
-
-        document.getElementById('showParticipants').style.visibility = 'visible';
-
+      });
+      
+      function getParameter(name) {
         var pageUrl = window.location.search.substring(1);
         var urlParams = pageUrl.split('&');
-        console.log(urlParams);
+        
         for (var i = 0; i < urlParams.length; i++) {
           var param = urlParams[i].split('=');
-          if (param[0] == 'gd') {
+          if (param[0] == name) {
             alert (param[1]);
           }
         }
-
-        function sendHangoutsUrl() {
-          $.ajax({
-            contentType: 'application/json',
-            type: 'POST',
-            url: "http://127.0.0.1:8000/lms299/groupchat/hangouts_url_for_session.json",
-            data: '{ "sessionId": "1", "hangoutsUrl": "' + gapi.hangout.getHangoutUrl() + '"}',
-            dataType: 'json'
-          });
-        }
-      });
+      }
+      
+      function sendHangoutsUrl() {
+        $.ajax({
+          contentType: 'application/json',
+          type: 'POST',
+          url: "http://127.0.0.1:8000/lms299/groupchat/hangouts_url_for_session.json",
+          data: '{ "sessionId": "1", "hangoutsUrl": "' + gapi.hangout.getHangoutUrl() + '"}',
+          dataType: 'json'
+        });
+      }
+        
+      function setUserChatSettings(cameraEnabled, micEnabled) {
+        gapi.hangout.av.setCameraMute(cameraEnabled);
+        gapi.hangout.av.setMicrophoneMute(micEnabled);
+      }
     }
   }
 }
