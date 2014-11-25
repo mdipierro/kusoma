04b83f6573e409563b3742b8b5d4da3f8d1800b3
Sara El Koutbia <elkoutbs@tcd.ie>
2014-11-25 17:40:42 -0600

    Adding code to retrieve the recent hangouts for the chat_hangout_section table and pass them to the view

--- controllers/groupchat.py
+++ controllers/groupchat.py
@@ -1,11 +1,19 @@
 def group_chat():
     return dict()
 
+
 @auth.requires_login()
 def google_hangouts():
-    courses = db(db.membership.auth_user == auth.user.id).select()
+    memberships = db(db.membership.auth_user == auth.user.id).select()
+    note_lists = []
+    for membership in memberships:
+        chat_sessions = db(db.group_chat_session.course_section == membership.course_section).select(
+            db.group_chat_session.course_section, db.group_chat_session.url, db.group_chat_session.start_time)
+        note_lists.append(chat_sessions)
+
     response.files.insert(0, URL('static', 'css/chat.css'))
-    return dict(courses=courses)
+    return dict(memberships=memberships, ids=note_lists)
+
 
 @auth.requires_login()
 def hangouts_url_for_session():
@@ -15,21 +23,25 @@ def hangouts_url_for_session():
 	for the hangout. the user's settings are also loaded into the database.
 	"""
     import gluon.contrib.simplejson as simplejson
+
     data = simplejson.loads(request.body.read())
     insert_new_hangout(data['course_section_id'], data['user_id'], data['hangoutUrl']);
     '''settings = get_user_group_chat_settings()'''
     '''update_user_group_chat_settings(settings.use_microphone, settings.use_camera)'''
     return dict(data)
-	
+
+
 @auth.requires_login()
 def update_user_settings_microphone():
     """
 	updates the user settings for the microphone
 	"""
     import gluon.contrib.simplejson as simplejson
+
     data = simplejson.loads(request.body.read())
     update_user_setting_mic(data[muteMicrophone])
     return dict(data)
+
 
 @auth.requires_login()
 def update_user_settings_camera():
@@ -37,9 +49,11 @@ def update_user_settings_camera():
     updates the user settings for the camera
 	"""
     import gluon.contrib.simplejson as simplejson
+
     data = simplejson.loads(request.body.read())
     update_user_setting_cam(data[muteCamera])
     return dict(data)
+
 
 @auth.requires_login()
 def insert_new_hangout(course_section_id, user_id, hangout_url):
@@ -47,13 +61,15 @@ def insert_new_hangout(course_section_id, user_id, hangout_url):
 	inserts a new hangout with the passed in course section
 	"""
     return dict(session_id=init_group_chat_session(course_section_id, user_id, hangout_url))
-	
+
+
 @auth.requires_login()
-def update_existing_hangout(session_id,  url):
+def update_existing_hangout(session_id, url):
     """
 	updates an existing hangout with the url to join that hangout
 	"""
     update_group_chat_session(session_id, url)
+
 
 @auth.requires_login()
 def history():
@@ -61,6 +77,7 @@ def history():
 	returns the chat sessions the user belongs to
 	"""
     return dict(sessions=get_group_chat_sessions_for_user())
+
 
 @auth.requires_login()
 def add_user_to_chat(session_id):
